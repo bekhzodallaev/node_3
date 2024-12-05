8171539f09a594deea2e43daffaf912ccbae9c25
Bekhzod Allaev <icebreaker0210mail.ru>
2024-12-05 12:39:13 +0100

    add delete functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { readAll } = require('./controllers/films-readall');
 const { readById } = require('./controllers/films-readById');
 const { createFilms } = require('./controllers/films-create');
 const { updateFilm } = require('./controllers/films-update');
+const { deleteFilm } = require('./controllers/films-delete');
 
 app.use(express.json());
 
@@ -11,6 +12,7 @@ app.get('/api/films/readall', readAll);
 app.get('/api/films/read', readById);
 app.post('/api/films/create', createFilms);
 app.put('/api/films/update', updateFilm);
+app.delete('/api/films/delete', deleteFilm);
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
